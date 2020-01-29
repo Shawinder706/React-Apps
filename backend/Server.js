ff976ca985fd4047e087a43a7f4c7ac3f86f5ff6
Shawinder706 <shawinder771@gmail.com>
2020-01-29 16:37:32 +0530

    add todos router

--- backend/Server.js
+++ backend/Server.js
@@ -2,51 +2,83 @@ const Express = require('express');
 const App = Express();
 const bodyParser = require('body-parser');
 const cors = require('cors')
-const todoRoutes = Express.Router()
-const PORT = 4000;
+const ToDoRouter = Express.Router()
 const ToDo = require('./todoModel')
+const PORT = 4000;
 
 const mongoose = require('mongoose')
+// establish mongodb connection
+const connect = mongoose.connect('mongodb://localhost:27017/todos');
 
 App.use(cors());
 App.use(bodyParser.json());
-// establish mongodb connection
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true })
-const Connection = mongoose.connection
-Connection.once('open', () => {
-    console.log("connection is established")
+
+connect.then((db) => {
+    console.log("connected correctly to server")
+}), (err) => {
+    console.log(err)
+}
+
+//***************************************************//
+App.use('/', ToDoRouter)
+
+//create new todos
+ToDoRouter.route('/create').post((req, res) => {
+    ToDo.create(req.body)
+        .then((todo) => {
+            res.statusCode = 200;
+            res.setHeader('content-type', 'aplication/json');
+            res.json(todo);
+        }), (err) => {
+            res.statusCode = 404;
+            console.log(err);
+        }
 })
 
-//get todos list
-todoRoutes.route('/').get((req, res) => {
-    ToDo.find((err, todos) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.json(todos)
+//show all todos list
+ToDoRouter.route('/').get((req, res) => {
+    ToDo.find({})
+        .then((todos) => {
+            res.statusCode = 200;
+            res.setHeader('content-type', 'application/json');
+            res.json(todos);
+        }), (err) => {
+            res.statusCode = 404;
+            console.log(err);
         }
-    });
 });
+
 // get todo by id
-todoRoutes.route('/:id').get((req, res) => {
-    ToDo.findById(req.params.id, (err, todos) => {
-        if (err) {
+ToDoRouter.route('/:id').get((req, res) => {
+    ToDo.findById(req.params.id)
+        .then((todo) => {
+            res.statusCode = 200;
+            res.setHeader('content-type', 'application/json')
+            res.json(todo);
+        }), (err) => {
+            res.statusCode = 404
             console.log(err)
-        } else {
-            res.json(todos)
         }
-    })
-})
-// add new todo
-todoRoutes.route('/add').post((req, res) => {
-    const todo = new ToDo(req.body);
-    todo.save()
-        .then(todo => {
-            res.status(200);
-            res.json('todo': 'to do added successfully')
+});
+
+// update todo 
+ToDoRouter.route('/update/:id').put((req, res, next) => {
+    ToDo.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+        .then((todo) => {
+            res.statusCode = 200;
+            res.setHeader('content-type', 'application/json')
+            res.json(todo)
+        }).catch((err) => {
+            next(err)
         })
 })
-App.use('/todos', todoRoutes)
+
+
+
+
+
+//App.get('/', (req, res) => res.send('Hello World!'))
+
 
 
 App.listen(PORT, () => console.log("server is running on " + PORT))
